3eafae894305d3d5037630c1b19696bd9a089bcc
danutdotrar <morarasu.danut@gmail.com>
2023-06-09 11:31:20 +0300

    Add showPosts fn

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ const filter = document.getElementById("filter");
 let limit = 3;
 let page = 1;
 
-// Get the post
-async function getPost() {
+// Fetch the post from API
+async function getPosts() {
     // Await fetch request
     const res = await fetch(
         `https://jsonplaceholder.typicode.com/posts?_limit=${limit}_page=${page}`
@@ -19,3 +19,15 @@ async function getPost() {
     // This will return as a promise, so when we'll use it we have to use async/await
     return data;
 }
+
+// Show posts in DOM
+async function showPosts() {
+    // getPosts returns a promise so we need to await on that
+    const posts = await getPosts();
+
+    // For each post, create a new element
+    posts.forEach((post) => {});
+}
+
+//Show initial posts
+showPosts();
